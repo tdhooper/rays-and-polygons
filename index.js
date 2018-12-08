1d6300cb051706b6024d8c5721a30119ef4054d2
Thomas Hooper <tom@stainlessvision.com>
2018-12-08 00:21:52 +0000

    Use the real fov and aspect

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ const setup = regl({
   },
 });
 
+// 1 x 1 x 1 Box
 const mesh = createCube();
+
 
 const drawPolygons = regl({
   vert: `
@@ -70,40 +72,53 @@ const drawRaymarch = regl({
     }
 
     float map(vec3 p) {
-      float d = length(p) - .1;
-      d = fBox(p, vec3(.5));
+      // 1 x 1 x 1 Box
+      float d = fBox(p, vec3(.5));
+
       p = mod(p, 1.) - .5;
-      d = min(d, length(p) - .2);
+      d = min(d, length(p) - .02);
       return d;
     }
 
-    const float ITER = 100.;
+    vec3 calcNormal(vec3 p) {
+      vec3 eps = vec3(.001,0,0);
+      vec3 n = vec3(
+        map(p - eps.xyy) - map(p + eps.xyy),
+        map(p - eps.yxy) - map(p + eps.yxy),
+        map(p - eps.yyx) - map(p + eps.yyx)
+      );
+      return normalize(n);
+    }
+
+    const float ITER = 50.;
 
     void main() {
-      float fovy = resolution.x / resolution.y;
-      vec2 uv = (gl_FragCoord.xy / resolution - .5) * vec2(fovy, 1);
+      float aspect = resolution.x / resolution.y;
+      vec2 uv = (gl_FragCoord.xy / resolution - .5) * vec2(aspect, 1);
+
       if (uv.x > 0.) {
         discard;
       }
 
-      // vec3 rayOrigin = vec3(0, 0, -1);
-      // vec3 rayDirection = vec3(uv, 1);
+      vec4 translation = view[3];
+      vec3 rayOrigin = (translation * view).xyz;
 
-      vec3 rayOrigin = vec4(vec4(0, 0, 3.333, 1) * view).xyz;
-      vec3 rayDirection = vec4(vec4(uv, -1, 1) * view).xyz;
+      float fov = projection[1].y * .5;
+      vec3 rayDirection = vec4(vec4(-uv, fov, 1) * view).xyz;
       rayDirection = normalize(rayDirection);
 
       float distance;
-      float accumilate = 0.;
+      vec3 color = vec3(0);
       for (float i = 0.; i < ITER; i++) {
         distance = map(rayOrigin);
         rayOrigin += rayDirection * distance;
-        accumilate += .01;
-        if (distance < .00001) {
+        color += .05;
+        if (distance < .001) {
+          color *= calcNormal(rayOrigin) * .5 + .5;
           break;
         }
       }
-      gl_FragColor = vec4(vec3(accumilate), 1);
+      gl_FragColor = vec4(color, 1);
     }
   `,
   attributes: {
@@ -117,16 +132,6 @@ const drawRaymarch = regl({
   uniforms: {
     model: mat4.identity([]),
     resolution: context => [context.viewportWidth, context.viewportHeight],
-    // projection: ({ viewportWidth, viewportHeight }) => {
-    //   const p = mat4.perspective(
-    //     [],
-    //     Math.PI / 5,
-    //     viewportWidth / viewportHeight,
-    //     0.01,
-    //     1000,
-    //   );
-    //   return mat4.invert(p, p);
-    // },
   },
 });
 
